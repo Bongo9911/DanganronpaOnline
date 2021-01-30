b04cdb9f2dc4e4dfa6bc581dc2cccf8bd02e9bc8
Bongo9911 <dawsonbuist@gmail.com>
2021-01-30 16:00:30 -0500

    Initial app creation

---
+++ src/app/credentials.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyAg-qfGaL_tzYNGOnWmJevd2lBP5kN6c5E",
+	authDomain: "danganronpaonline-545fa.firebaseapp.com",
+	projectId: "danganronpaonline-545fa",
+	storageBucket: "danganronpaonline-545fa.appspot.com",
+	messagingSenderId: "390013565471",
+	appId: "1:390013565471:web:8da4232b1d6f2a126f332a",
+	measurementId: "G-XYDB800650"
+  };
