bf4e00c87e4362a97d43c6ab659025841d230a78
Bongo9911 <dawsonbuist@gmail.com>
2021-02-01 10:36:54 -0500

    laid out basic page and added a chat feature and player list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { TOUCH_BUFFER_MS } from '@angular/cdk/a11y';
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase';
@@ -11,26 +10,39 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'Danganronpa Online';
-  gameID : string = '';
-  enteredID : string = '';
-  name : string = '';
+
+  gameID: string = 'DYUN';
+  room: string = 'Lobby';
+
+  enteredID: string = '';
+  name: string = 'Bongo';
+  message: string;
+
+  player$: Player[];
+  message$: Chat[];
 
   constructor(private firestore: AngularFirestore) {
-    //this.getMessages().subscribe(result => this.message$ = result);
+    this.firestore.collection<Game>('games').doc(this.gameID)
+      .collection<Player>('players').valueChanges()
+      .subscribe(result => this.player$ = result);
+
+    this.firestore.collection<Game>('games').doc(this.gameID)
+      .collection<Chat>('chat', ref => ref.orderBy('timestamp')).valueChanges()
+      .subscribe(result => this.message$ = result);
   }
 
   // getMessages() : Observable<FirestoreRec[]>{
   //   //return this.firestore.collection<FirestoreRec>('messages', ref => ref.orderBy('timestamp')).valueChanges()
   // }
 
-  createGame() : void {
+  createGame(): void {
     //TODO: Add check for duplicate IDs
     //TODO: Add timeout for games with no activity
     console.log("Test");
     let resultID = '';
     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let charactersLength = characters.length;
-    for(let i = 0; i < 4; ++i) { //generates a random 4-character game id
+    for (let i = 0; i < 4; ++i) { //generates a random 4-character game id
       resultID += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     this.firestore.collection<Game>('games').doc(resultID).set({
@@ -48,18 +60,67 @@ export class AppComponent {
     this.gameID = resultID;
   }
 
-  joinGame() : void {
+  joinGame(): void {
+    this.enteredID = this.enteredID.toUpperCase();
+    const usersRef = this.firestore.collection('games').doc(this.enteredID).get().toPromise().then(async (docSnapshot) => {
+      if (await docSnapshot.exists) {
+        this.firestore.collection<Game>('games').doc(this.enteredID)
+          .collection<Player>('players').get().toPromise().then(snap => {
+            let size = snap.size // will return the collection size
+            if (size < 16) { //checks to make sure player cap hasn't been reached
+              //TODO: Check if a player in the lobbby already has the name you want
+              this.gameID = this.enteredID;
+              this.firestore.collection<Game>('games').doc(this.gameID)
+                .collection<Player>('players').doc(this.name).set({
+                  name: this.name,
+                });
 
+              this.firestore.collection<Game>('games').doc(this.gameID)
+                .collection<Player>('players').valueChanges()
+                .subscribe(result => this.player$ = result);
+
+              this.firestore.collection<Game>('games').doc(this.gameID)
+                .collection<Chat>('chat', ref => ref.orderBy('timestamp')).valueChanges()
+                .subscribe(result => this.message$ = result);
+            }
+            else {
+              //TODO: Add error message for full lobby
+            }
+          });
+      }
+      else {
+        //TODO: Add error message that lobby doesn't exist
+      }
+    });
+  }
+
+  sendMessage() : void {
+    this.firestore.collection<Game>('games').doc(this.gameID).collection<Chat>('chat').add({
+      message: this.name + ': ' + this.message,
+      room: this.room,
+      gametime: 0,
+      day: 0,
+      timestamp: new firebase.default.firestore.Timestamp(Date.now() / 1000, 0),
+    });
+    this.message = "";
   }
 }
 
 interface Game {
-  ID : string
-  Phase : string
-  Time : Number
-  Day : Number
+  ID: string
+  Phase: string
+  Time: Number
+  Day: Number
 }
 
 interface Player {
   name: string
+}
+
+interface Chat {
+  message: string
+  room: string
+  gametime: number
+  day: number
+  timestamp: firebase.default.firestore.Timestamp
 }
